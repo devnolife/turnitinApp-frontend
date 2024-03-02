57174a70dfb07a7e12cdba2f82aa131cc0311a4d
devnolife <andi_agung@student.unismuh.ac.id>
2024-03-02 21:06:14 +0800

    perubahan

--- src/@core/auth/jwt/jwtDefaultConfig.js
+++ src/@core/auth/jwt/jwtDefaultConfig.js
@@ -1,8 +1,6 @@
 // ** Auth Endpoints
 
-export const baseUrl = 'http:///192.168.1.20:8080'
-// export const baseUrl = 'http://103.186.1.3:8080'
-// export const baseUrl = 'http://10.0.8.104:8080'
+export const baseUrl = 'http://localhost:8080'
 export default {
 
   loginEndpoint: `${baseUrl}/api/login`,
